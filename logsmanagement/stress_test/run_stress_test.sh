5876edc881ed96cd059d6d1eae995e11909ba1b4
Dim-P <dimitris1703@gmail.com>
2021-04-24 05:09:58 +0100

    Change stress test netdata install path to /tmp

--- logsmanagement/stress_test/run_stress_test.sh
+++ logsmanagement/stress_test/run_stress_test.sh
@@ -8,10 +8,10 @@ sudo systemctl stop netdata
 echo "=========================================="
 echo "Removing existing simulated netdata logs-management files..."
 sudo rm -rf /tmp/netdata_log_management_stress_test_data
-echo "Removing DBs /var/cache/netdata/logs_management_db/* ..."
-sudo rm -rf /var/cache/netdata/logs_management_db
-echo "Removing potential existing /etc/netdata/log_management.conf ..."
-sudo rm -f /etc/netdata/log_management.conf
+echo "Removing DBs /tmp/netdata/var/cache/netdata/logs_management_db/* ..."
+sudo rm -rf /tmp/netdata/var/cache/netdata/logs_management_db
+echo "Removing potential existing /tmp/netdata/etc/netdata/log_management.conf ..."
+sudo rm -f /tmp/netdata/etc/netdata/log_management.conf
 echo "Removing /tmp/netdata-logs-stress-test pipe ..."
 sudo rm -f /tmp/netdata-logs-stress-test
 
@@ -19,14 +19,14 @@ echo "=========================================="
 echo "Rebuilding Netdata with stress tests for logs-management enabled..."
 echo "=========================================="
 cd ../../..
-yes | sudo ./netdata-uninstaller.sh --yes --env /etc/netdata/.environment
-sudo rm -rf /etc/netdata # Remove /etc/netdata if it persists for some reason
+yes | sudo ./netdata-uninstaller.sh --yes --env /tmp/netdata/etc/netdata/.environment
+sudo rm -rf /tmp/netdata/etc/netdata # Remove /etc/netdata if it persists for some reason
 cd netdata
-sudo CFLAGS="-O1 -ggdb -DNETDATA_INTERNAL_CHECKS=1 -DDEBUG_LEV=2 -DLOGS_MANAGEMENT_STRESS_TEST=1" ./netdata-installer.sh --dont-start-it --disable-cloud --disable-ebpf --disable-go --enable-logsmanagement
+sudo CFLAGS="-O1 -ggdb -DNETDATA_INTERNAL_CHECKS=1 -DDEBUG_LEV=2 -DLOGS_MANAGEMENT_STRESS_TEST=1" ./netdata-installer.sh --dont-start-it --disable-cloud --disable-ebpf --disable-go --enable-logsmanagement --install /tmp
 # make && sudo make install
 
 cd logsmanagement/stress_test
-sudo cp log_management.conf /etc/netdata
+sudo cp log_management.conf /tmp/netdata/etc/netdata
 
 sudo -u netdata -g netdata mkdir /tmp/netdata_log_management_stress_test_data
 #for (( i = 0; i < $number_log_sources; i++ )) 
@@ -42,4 +42,4 @@ gcc stress_test.c -luv -o stress_test
 sudo -u netdata -g netdata ./stress_test "$number_log_sources" &
 sleep 2
 #sudo systemctl start netdata
-sudo -u netdata -g netdata -s gdb -ex=run --args /usr/sbin/netdata -D
+sudo -u netdata -g netdata -s gdb -ex=run --args /tmp/netdata/usr/sbin/netdata -D
